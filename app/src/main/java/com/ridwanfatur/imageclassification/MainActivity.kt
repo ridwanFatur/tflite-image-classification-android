0eac11e043dd043e6166ecfbc82e169d4b1f7c30
ridwanfatur98 <ridwan@knowledgecatalyst.io>
2026-02-19 12:45:53 +0700

    Init Project

---
+++ app/src/main/java/com/ridwanfatur/imageclassification/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.ridwanfatur.imageclassification
+
+import android.Manifest
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Display
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.ridwanfatur.imageclassification.databinding.ActivityMainBinding
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import java.text.NumberFormat
+import java.util.Locale
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var doubleBackToExitPressedOnce = false
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            val message = if (isGranted) "Camera permission granted" else "Camera permission rejected"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+
+        startCamera()
+
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+        android.os.Handler(mainLooper).postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
+    }
+
+
+
+    private fun startCamera() {
+
+        imageClassifierHelper =
+            ImageClassifierHelper(
+                context = this,
+                imageClassifierListener = object : ImageClassifierHelper.ClassifierListener {
+                    override fun onError(error: String) {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                        runOnUiThread {
+                            results?.let { it ->
+                                if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
+                                    println(it)
+                                    val sortedCategories =
+                                        it[0].categories.sortedByDescending { it?.score }
+                                    val displayResult =
+                                        sortedCategories.joinToString("\n") {
+                                            "${it.label} " + NumberFormat.getPercentInstance().format(it.score).trim()
+                                        }
+                                    binding.tvResult.text = displayResult
+                                }
+                            }
+                        }
+                    }
+                })
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val imageAnalyzer =
+                ImageAnalysis.Builder()
+                    .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                    .setTargetRotation(binding.viewFinder.display.rotation)
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                    .build()
+                    .also {
+                        it.setAnalyzer(Executors.newSingleThreadExecutor()) { image ->
+                            imageClassifierHelper.classify(image)
+                        }
+                    }
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    CameraSelector.DEFAULT_BACK_CAMERA,
+                    preview,
+                    imageAnalyzer
+                )
+            } catch (exc: Exception) {
+                Toast.makeText(this, "Failed to bind camera use case", Toast.LENGTH_SHORT).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+}
